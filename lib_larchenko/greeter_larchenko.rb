421ae62c15bc7a5a6ed45747ee6968892fb3b718
serglarchenko <42350082+serglarchenko@users.noreply.github.com>
2018-08-14 16:25:25 +0300

    comm

--- lib_larchenko/greeter_larchenko.rb
+++ lib_larchenko/greeter_larchenko.rb
@@ -3,13 +3,7 @@ class Greeter
     @who = who
   end
   def greet
-<<<<<<< HEAD
+
     "Hello, World and Larchenko Serhii #{@who}"
   end
 end
-
-=======
-    "Hello World , Me  and others, #{@who}"
-  end
-end
->>>>>>> lib_larchenko
